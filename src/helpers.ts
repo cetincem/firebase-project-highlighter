63dbfb64600b7a0050d2501c68a2766372014a37
Cetin Cem <cetincem.atabey@gmail.com>
2024-06-04 01:45:34 +0300

    chore: Refactor helpers.ts

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,34 +1,51 @@
 import * as vscode from 'vscode';
 import { exec } from 'child_process';
 
-export function getFirebaseProjectName(): Promise<string> {
+function getFirebaseProjectName(): Promise<string> {
     return new Promise((resolve, reject) => {
-        exec('firebase use --add', (error, stdout, stderr) => {
+        exec('firebase use --json', (error, stdout, stderr) => {
             if (error) {
+                console.error(`Error executing firebase command: ${stderr}`);
                 vscode.window.showErrorMessage(`Error fetching Firebase project: ${stderr}`);
-                reject(error);
+                reject(stderr);
                 return;
             }
 
-            const match = stdout.match(/projects\/([^:]+)/);
-            if (match) {
-                resolve(match[1]);
-            } else {
-                reject('No project found');
+            console.log(`Firebase command output: ${stdout}`);
+            
+            try {
+                const jsonOutput = JSON.parse(stdout);
+                if (jsonOutput.status === 'success' && jsonOutput.result) {
+                    resolve(jsonOutput.result);
+                } else {
+                    reject('No project found or invalid response format');
+                }
+            } catch (parseError) {
+                console.error(`Error parsing JSON output: ${parseError}`);
+                reject('Error parsing JSON output');
             }
         });
     });
 }
 
+
 export function changeThemeBasedOnProject(projectName: string) {
     let theme: string;
+    console.info('changing theme based on project');
+    console.log('projectName:', projectName);
 
     switch (projectName) {
-        case 'project-one':
+        case 'vickyparking-dev':
+        case 'development':
             theme = 'Abyss';
             break;
-        case 'project-two':
+        case 'vickyparking-demo':
+        case 'demo':
             theme = 'Monokai';
+            break;
+        case 'vickyparking-prod':
+        case 'production':
+            theme = 'Default Light+';
             break;
         default:
             theme = 'Default Dark+';
